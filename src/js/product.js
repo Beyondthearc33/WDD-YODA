f84ffbabb6a399d8861ef4376add6fb0910615d5
Beyondthearc33 <146131473+Beyondthearc33@users.noreply.github.com>
2026-05-08 16:23:44 -0600

    week 3 group code

--- src/js/product.js
+++ src/js/product.js
@@ -1,31 +1,5 @@
-import { setLocalStorage, getParam } from "./utils.mjs";
-import { findProductById } from "./productData.mjs";
-
-function addProductToCart(product) {
-  let cart = JSON.parse(localStorage.getItem("so-cart")) || [];
-
-  if (!cart) {
-    cart = [];
-  }
-
-  if (!Array.isArray(cart)) {
-    cart = [];
-  }
-
-  cart.push(product);
-  setLocalStorage("so-cart", cart);
-}
-// add to cart button event handler
-async function addToCartHandler(e) {
-  const product = await findProductById(e.target.dataset.id);
-  addProductToCart(product);
-}
-
-// add listener to Add to Cart button
-document
-  .getElementById("addToCart")
-  .addEventListener("click", addToCartHandler);
+import { getParam } from "./utils.mjs";
+import productDetails from "./productDetails.mjs";
 
 const productId = getParam("product");
-
-console.log(findProductById(productId));
+productDetails(productId);

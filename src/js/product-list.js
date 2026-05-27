9eb5fc99fad8b29c22169429092f9ffb1ecd0bbb
colebeebe <bee14003@byui.edu>
2026-05-27 17:49:28 -0400

    Finished team05 + stretch goals

--- src/js/product-list.js
+++ src/js/product-list.js
@@ -3,4 +3,10 @@ import productList from "./productList.mjs";
 
 const category = getParam("category");
 
+function unslug(str) {
+  return str.replace(/-/g, " ").replace(/\b\w/g, c => c.toUpperCase());
+}
+
+document.querySelector("#top-products").textContent = `Top Products - ${unslug(category)}`;
+
 productList(".product-list", category);
